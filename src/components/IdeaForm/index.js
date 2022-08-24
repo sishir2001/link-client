50c17aa7b2101be66e5bdf00d722a3e83f9c0d43
Jaladi Saai Sishir <54372985+sishir2001@users.noreply.github.com>
2022-08-24 21:39:41 +0530

    before merge with dev

--- src/components/IdeaForm/index.js
+++ src/components/IdeaForm/index.js
@@ -1,13 +1,40 @@
+import { makeStyles, Typography } from "@material-ui/core";
 import React from "react";
 import globalUseStyles from "../../GlobalStyle";
+import ideaBulbSVG from "../../svgs/ideaBulb.svg";
+import { Pagination } from "antd";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+        padding: theme.spacing(1),
+    },
+    leftHeader: {
+        flexGrow: 1,
+    },
+}));
 
 const IdeaForm = (props) => {
     const globalClasses = globalUseStyles();
+    const classes = useStyles();
+
+    const renderIdeaFormHeader = () => {
+        return (
+            <div className={classes.root}>
+                <div className={classes.leftHeader}>
+                    <img src={ideaBulbSVG} alt="idea bulb logo" />
+                    <Typography variant="h4">
+                        Idea Submission<span>.</span>
+                    </Typography>
+                </div>
+                <Pagination defaultCurrent={1} total={50} />
+            </div>
+        );
+    };
 
     return (
         <div>
             <div className={globalClasses.toolbar}></div>
-            <div>Idea From</div>
+            <div>{renderIdeaFormHeader()}</div>
         </div>
     );
 };
