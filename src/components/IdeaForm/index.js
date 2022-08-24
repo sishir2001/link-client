a14f8b330661a9ff37f913f30507e0cd7fc866e3
Jaladi Saai Sishir <54372985+sishir2001@users.noreply.github.com>
2022-08-24 22:38:12 +0530

    add : header

--- src/components/IdeaForm/index.js
+++ src/components/IdeaForm/index.js
@@ -1,40 +1,75 @@
-import { makeStyles, Typography } from "@material-ui/core";
+import { makeStyles, Typography, Divider } from "@material-ui/core";
 import React from "react";
 import globalUseStyles from "../../GlobalStyle";
 import ideaBulbSVG from "../../svgs/ideaBulb.svg";
 import { Pagination } from "antd";
+import { useState } from "react";
 
 const useStyles = makeStyles((theme) => ({
     root: {
-        padding: theme.spacing(1),
+        paddingTop: theme.spacing(2),
+        paddingBottom: theme.spacing(2),
+    },
+    divider: {
+        marginTop: theme.spacing(2),
+        marginBottom: theme.spacing(2),
+    },
+    pagination: {
+        borderColor: theme.palette.primary.main,
+    },
+    HeaderRoot: {
+        display: "flex",
+        alignItems: "center",
     },
     leftHeader: {
+        display: "flex",
         flexGrow: 1,
+        alignItems: "center",
+        justifyContent: "flex-start",
+    },
+    HeaderTitle: {
+        paddingLeft: theme.spacing(1),
+        fontWeight: 800,
+    },
+    TitleSpan: {
+        color: "#EE5066",
     },
 }));
 
 const IdeaForm = (props) => {
     const globalClasses = globalUseStyles();
     const classes = useStyles();
+    const [page, setPage] = useState(1);
+    const onChange = (pageNo) => {
+        setPage(pageNo);
+    };
 
     const renderIdeaFormHeader = () => {
         return (
-            <div className={classes.root}>
+            <div className={classes.HeaderRoot}>
                 <div className={classes.leftHeader}>
                     <img src={ideaBulbSVG} alt="idea bulb logo" />
-                    <Typography variant="h4">
-                        Idea Submission<span>.</span>
+                    <Typography variant="h4" className={classes.HeaderTitle}>
+                        Idea Submission
+                        <span className={classes.TitleSpan}>.</span>
                     </Typography>
                 </div>
-                <Pagination defaultCurrent={1} total={50} />
+                <Pagination
+                    size="large"
+                    onChange={onChange}
+                    defaultCurrent={1}
+                    className={classes.pagination}
+                    total={50}
+                />
             </div>
         );
     };
 
     return (
-        <div>
+        <div className={`${globalClasses.toolbarMargin} ${classes.root}`}>
             <div className={globalClasses.toolbar}></div>
             <div>{renderIdeaFormHeader()}</div>
+            <Divider className={classes.divider} />
         </div>
     );
 };
