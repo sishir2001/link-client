c4c548cebc49133d19e7c920cce99204b0e22a8f
Jaladi Saai Sishir <54372985+sishir2001@users.noreply.github.com>
2022-08-26 15:14:49 +0530

    add : roles onboard

--- src/components/Authentication/SelectRole.js
+++ src/components/Authentication/SelectRole.js
@@ -1,23 +1,27 @@
-import React from 'react';
-import { makeStyles, Button, Typography} from "@material-ui/core";
+import React, { useState } from "react";
+import { makeStyles, Button, Typography } from "@material-ui/core";
 import Dot from "../../svgs/dot.svg";
-import ArrowForwardIcon from '@material-ui/icons/ArrowForward';
+import ArrowForwardIcon from "@material-ui/icons/ArrowForward";
+import _ from "lodash";
+import { useSelector } from "react-redux";
+import { API, API_HEADER } from "../../global";
+import { useHistory } from "react-router-dom";
 
 const useStyles = makeStyles((theme) => ({
     toolbar: theme.mixins.toolbar,
     root: {
         height: "100vh",
         width: "100vw",
-        margin: 'auto',
+        margin: "auto",
         padding: 0,
         maxWidth: "100%",
         overflowX: "hidden",
         backgroundImage:
             "linear-gradient(to top, #051937, #002158, #002779, #002a9a, #3028b8);",
-        display: 'flex',
+        display: "flex",
         flexDirection: "column",
-        alignItems: 'center',
-        justifyContent: 'center',
+        alignItems: "center",
+        justifyContent: "center",
     },
     heading: {
         display: "flex",
@@ -35,32 +39,40 @@ const useStyles = makeStyles((theme) => ({
     img1: {
         marginTop: theme.spacing(6),
     },
-    div1:{
+    div1: {
         display: "flex",
         justifyContent: "center",
         marginTop: "80px",
     },
     role: {
-        width: '250px',
-        height: '210px',
-        background: '#FFFFFF',
+        width: "250px",
+        height: "210px",
+        background: "#FFFFFF",
         textTransform: "initial",
-        boxShawdow: '0px 4px 4px rgba(0, 0, 0, 0.25)',
-        borderRadius: '24px',
-        background: "white",
+        boxShawdow: "0px 4px 4px rgba(0, 0, 0, 0.25)",
+        borderRadius: "24px",
         display: "flex",
         alignItems: "center",
         justifyContent: "center",
-        color: "#0B2F8A",
         fontFamily: "Poppins",
         fontWeight: 600,
         fontStyle: "normal",
         fontSize: "28px",
         marginRight: "40px",
     },
+    roleSelected: {
+        background:
+            "linear-gradient(93.45deg, #EE5066 0%, #B25A59 99.99%, #B25A59 100%)",
+        color: "white",
+    },
+    roleNotSelected: {
+        background: "white",
+        color: "#0B2F8A",
+    },
     button: {
         color: "white",
-        background: "linear-gradient(93.45deg, #EE5066 0%, #B25A59 99.99%, #B25A59 100%)",
+        background:
+            "linear-gradient(93.45deg, #EE5066 0%, #B25A59 99.99%, #B25A59 100%)",
         textTransform: "initial",
         width: "12ch",
         fontSize: "18px",
@@ -83,8 +95,72 @@ const SelectRole = () => {
     //     setSelectedValue(event.target.value);
     //     console.log(event.target.value);
     // };
+    const [role, selectRole] = useState(null);
+    const [normalUser, selectNormalUser] = useState(false);
+    const [investor, selectInvestor] = useState(false);
+    const [organization, selectorganizations] = useState(false);
+    const [mentor, selectMentor] = useState(false);
+    const history = useHistory();
+    const { auth } = useSelector((state) => state);
+    const { jwtToken } = auth;
 
-    return(
+    const handleClick = (check) => (event) => {
+        event.preventDefault();
+        selectRole(check);
+        if (check === "normaluser") {
+            selectNormalUser(true);
+            selectInvestor(false);
+            selectorganizations(false);
+            selectMentor(false);
+        } else if (check === "investor") {
+            selectNormalUser(false);
+            selectInvestor(true);
+            selectorganizations(false);
+            selectMentor(false);
+        } else if (check === "organization") {
+            selectNormalUser(false);
+            selectInvestor(false);
+            selectorganizations(true);
+            selectMentor(false);
+        } else if (check === "mentor") {
+            selectNormalUser(false);
+            selectInvestor(false);
+            selectorganizations(false);
+            selectMentor(true);
+        }
+        // console.log(role);
+    };
+
+    const postRoleAPI = async () => {
+        const formData = new FormData();
+        formData.append("role", role);
+        try {
+            // post the role to the API
+            const res = await fetch(`${API}/settings/set_get_role`, {
+                method: "POST",
+                body: formData,
+                headers: {
+                    Authorization: "Bearer " + jwtToken["access"],
+                },
+            });
+            const resJson = await res.json();
+            if (_.isUndefined(resJson.response)) {
+                // no matching role
+                console.log(resJson.detail);
+            } else {
+                history.push("/interests");
+            }
+        } catch (e) {
+            console.log(e);
+        }
+    };
+
+    const handleNext = () => {
+        console.log(role);
+        postRoleAPI();
+    };
+
+    return (
         <div className={classes.root}>
             <div className={classes.toolbar}></div>
             <div className={classes.heading}>
@@ -94,10 +170,15 @@ const SelectRole = () => {
                 <img className={classes.img1} src={Dot} alt="dot" />
             </div>
             <div className={classes.div1}>
-                <div >
+                <div>
                     <Button
-                        className={classes.role}
+                        className={`${classes.role} ${
+                            normalUser
+                                ? classes.roleSelected
+                                : classes.roleNotSelected
+                        }`}
                         variant="contained"
+                        onClick={handleClick("normaluser")}
                         style={{ borderRadius: 10 }}
                     >
                         Normal User
@@ -105,8 +186,13 @@ const SelectRole = () => {
                 </div>
                 <div>
                     <Button
-                        className={classes.role}
+                        className={`${classes.role} ${
+                            investor
+                                ? classes.roleSelected
+                                : classes.roleNotSelected
+                        }`}
                         variant="contained"
+                        onClick={handleClick("investor")}
                         style={{ borderRadius: 10 }}
                     >
                         Investor
@@ -114,18 +200,28 @@ const SelectRole = () => {
                 </div>
                 <div>
                     <Button
-                        className={classes.role}
+                        className={`${classes.role} ${
+                            organization
+                                ? classes.roleSelected
+                                : classes.roleNotSelected
+                        }`}
                         variant="contained"
+                        onClick={handleClick("organization")}
                         style={{ borderRadius: 10 }}
                     >
-                        Organisations
+                        organization
                     </Button>
                 </div>
                 <div>
                     <Button
-                        className={classes.role}
+                        className={`${classes.role} ${
+                            mentor
+                                ? classes.roleSelected
+                                : classes.roleNotSelected
+                        }`}
                         variant="contained"
                         style={{ borderRadius: 10 }}
+                        onClick={handleClick("mentor")}
                     >
                         Mentor
                     </Button>
@@ -135,12 +231,14 @@ const SelectRole = () => {
                 className={classes.button}
                 variant="contained"
                 style={{ borderRadius: 10 }}
-                endIcon={<ArrowForwardIcon/>}
+                endIcon={<ArrowForwardIcon />}
+                disabled={_.isNull(role)}
+                onClick={handleNext}
             >
                 Next
             </Button>
         </div>
-    )
-}
+    );
+};
 
 export default SelectRole;
--- src/global.js
+++ src/global.js
@@ -1,1 +1,10 @@
+import { useSelector } from "react-redux";
 export const API = "https://theprojectlink.herokuapp.com";
+
+export const API_HEADER = () => {
+    const { auth } = useSelector((state) => state);
+    const { jwtToken } = auth;
+    return {
+        Authorization: "Bearer " + jwtToken["access"],
+    };
+};
